32d326ac061e495e6f8de4037b24474a56d12e0b
Kevin Maduro <nivekzin@gmail.com>
2018-04-01 17:07:43 -0300

    Added .idea in .gitignore and created MangaListItem

--- src/manga/components/manga-list-item.tsx
+++ src/manga/components/manga-list-item.tsx
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-export default class MangaListItem extends Component {
+export class MangaListItem extends Component {
+  static propTypes = {
+    manga: PropTypes.shape({
+      name: PropTypes.string,
+      status: PropTypes.string,
+      progress: PropTypes.string,
+    }),
+  };
   
   renderItem() {
     return (
       <li>
-        <h3>Name</h3>
-        <p>Status</p>
-        <p>Progress</p>
+        <h3>{this.props.manga.name}</h3>
+        <p>{this.props.manga.status}</p>
+        <p>{this.props.manga.progress}</p>
       </li>
     );
   }
@@ -18,3 +27,8 @@ export default class MangaListItem extends Component {
     );
   }
 }
+
+export default connect(
+  state => ({ manga: state.manga }),
+  null,
+);
